9975ead230b3047174c93009aa9065d86eb74354
ajaykumarj1988 <66014687+ajaykumarj1988@users.noreply.github.com>
2020-05-29 15:18:48 +0530

    updated the Robot 1 class

--- src/test/java/robot/Robot1.java
+++ src/test/java/robot/Robot1.java
@@ -3,11 +3,13 @@ package robot;
 import java.awt.AWTException; 
 import java.awt.Robot; 
 import java.awt.event.KeyEvent; 
-import java.io.*; 
+import java.io.*;
+
+import org.testng.annotations.Test; 
   
 public class Robot1 
 { 
-    public static void main(String[] args) throws IOException, 
+    /*public static void main(String[] args) throws IOException, 
                            AWTException, InterruptedException 
     { 
         String command = "notepad.exe"; 
@@ -58,5 +60,57 @@ public class Robot1
         Thread.sleep(250); 
         robot.keyPress(KeyEvent.VK_Y); 
         
-    } 
+    } */
+    @Test
+    public void Demomethod() throws IOException, AWTException, InterruptedException {
+    	System.out.println("This is a Demo method");
+    	String command = "notepad.exe"; 
+        Runtime run = Runtime.getRuntime(); 
+        run.exec(command); 
+        try { 
+            Thread.sleep(2000); 
+        } 
+        catch (InterruptedException e) 
+        { 
+            // TODO Auto-generated catch block 
+            e.printStackTrace(); 
+        } 
+  
+        // Create an instance of Robot class 
+        Robot robot = new Robot(); 
+  
+        // Press keys using robot. A gap of 
+        // of 250 mili seconds is added after 
+        // every key press 
+        robot.keyPress(KeyEvent.VK_H); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_E); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_L); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_L); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_O); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_SPACE); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_F); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_R); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_O); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_M); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_SPACE); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_A); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_J); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_A); 
+        Thread.sleep(250); 
+        robot.keyPress(KeyEvent.VK_Y); 
+ 
+    }
 }
